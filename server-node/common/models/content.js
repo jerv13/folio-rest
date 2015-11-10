e0b424f6814aa98b4a6961216d5825680af7f40e
James Jervis <jjervis@relivinc.com>
2015-11-10 17:46:03 -0600

    Add node server

---
+++ server-node/common/models/content.js
@@ -0,0 +1,16 @@
+module.exports = function(Content) {
+
+    //var uuid = require('uuid');
+
+    console.log(Content);
+    /*
+    Content.beforeCreate(
+        'create',
+        function(context, user, next) {
+        var req = context.req;
+        req.body.id = uuid.v4();
+        req.body.date = Date.now();
+        next();
+    });
+    */
+};
