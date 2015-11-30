420c4057e59164c72d90dcedc6bceb5803769a42
James Jervis <jjervis@relivinc.com>
2015-11-30 12:28:34 -0600

    Styles

--- server-node/server/boot/root.js
+++ server-node/server/boot/root.js
@@ -1,6 +1,7 @@
 module.exports = function(server) {
+  var restApiRoot = server.get('restApiRoot');
   // Install a `/` route that returns server status
   var router = server.loopback.Router();
-  router.get('/', server.loopback.status());
+  router.get(restApiRoot, server.loopback.status());
   server.use(router);
 };
